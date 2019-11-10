06bcbfcd1e765c5a376ef873afb2e88a246f37c5
nlivaic <nlivaic@gmail.com>
2019-11-11 00:02:38 +0100

    chore: old files removed.

--- ShoppingBasket.Core/IShoppingBasketService.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace ShoppingBasket.Core
-{
-    public interface IShoppingBasketService
-    {
-        ShoppingBasket CreateShoppingBasket(IEnumerable<Item> items, IEnumerable<Discount> discounts);
-    }
-}
